be7f05dddfa694d65fcde354910aafca853ee41d
farxaddoctor <42151772+farxaddoctor@users.noreply.github.com>
2021-06-09 16:52:52 +0400

    Add files via upload
    Добавления файлов conftest.py и test_tems.py

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import  pytest
+from time import sleep
+link ='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_goods_has_add_to_basket_button(browser):
+    browser.get(link)
+    sleep(30)
+    add_to_basket = browser.find_element_by_css_selector('.btn-add-to-basket')
+    assert add_to_basket, 'Кнопка добавления в корзину отсутвует'
